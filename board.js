30beb00a78cce5fb9531f6215da993a6177302d3
viktoriotw <victorgarciavilu@gmail.com>
2015-07-30 13:13:50 -0700

    crud cli

--- board.js
+++ board.js
@@ -7,9 +7,23 @@ var hash = function(string){
     return string
 }
 
+var authenticate = function(name, password, callback){
+    Board.findOne({name:name}, function(e, board){
+        if(e) console.log("Error: ", e);
+        else{
+            if(hash(password) == board.password){
+                callback(board)
+            }else{
+                console.log("NOT ALLOWED");
+            }
+        }
+    });
+}
+
 program
     .version("0.0.1");
 
+// CREATE
 program
     .command("create <name> <password> <lat> <lon>")
     .description("creates a new board")
@@ -32,22 +46,31 @@ program
         });
     });
 
+// READ
 program
-.command("read <name> <password>")
+    .command("read <name> <password>")
     .description("read board information")
     .action(function(name, password){
-        Board.findOne({name: name}, function(e, board){
-            if(e) console.log("Error", e);
-            else{
-                if(hash(password) == board.password){
-                    console.log(board)
-                }else{
-                    console.log("NOT ALLOWED")
-                }
-            }
+
+        authenticate(name, password, function(name, password){
+            console.log(board)
             process.exit(0);
         });
     });
 
+//UPDATE
+program
+.command("update <name> <password>")
+    .description("update board")
+    .action(function(name, password){
+        authenticate(name, password, function(board){
 
+        });
+    });
+
+// DELETE
+program
+    .command()
+    .description()
+    .action();
 program.parse(process.argv);
